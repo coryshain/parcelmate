b63bbfa568d730cd8868f2f89dbd89a2e0536372
Cory Shain <cory.shain@gmail.com>
2025-01-17 00:54:21 -0800

    Made config optional

--- parcelmate/bin/main.py
+++ parcelmate/bin/main.py
@@ -6,14 +6,17 @@ from parcelmate.plot import *
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser('''Main executable for parcelmate package.''')
-    argparser.add_argument('config', help='Path to config file.')
+    argparser.add_argument('-c', '--config', default=None, help='Path to config file.')
     argparser.add_argument('-O', '--overwrite', action='store_true',
                            help='Recompute all outputs, even if they already exist.')
     args = argparser.parse_args()
     config = args.config
     overwrite = args.overwrite
 
-    cfg = get_cfg(config)
+    if config is not None:
+        cfg = get_cfg(config)
+    else:
+        cfg = {}
 
     run_connectivity(
         output_dir=cfg.get('output_dir', 'results'),
